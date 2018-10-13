63055bb88836e9daba6576d6a5db42f88a6dd9d8
Christopher Chalmers <c.chalmers@me.com>
2018-10-13 10:45:56 +0100

    Make start on diagrams-2.0
    I can't remember what state it's currently in.

--- src/Physics/ForceLayout.hs
+++ src/Physics/ForceLayout.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE BangPatterns        #-}
 {-# LANGUAGE TemplateHaskell     #-}
 {-# LANGUAGE TypeFamilies        #-}
 
@@ -52,47 +53,53 @@
 -----------------------------------------------------------------------------
 
 module Physics.ForceLayout
-       ( -- * Data structures
+  ( -- * Data structures
 
-         Particle(..), pos, vel, force
-       , initParticle
+    Particle(..), pos, vel -- , force
+  , initParticle
 
-       , PID
-       , Edge
-       , Ensemble(..), forces, particles
+  , PID
+  , Edge
+  , Ensemble(..), forces, particles
 
-         -- * Pre-defined forces
+    -- * Pre-defined forces
 
-       , hookeForce
-       , coulombForce
-       , distForce
+  , hookeForce
+  , coulombForce
+  , distForce
 
-         -- * Running simulations
+    -- * Running simulations
 
-       , ForceLayoutOpts(..)
-       , damping, energyLimit, stepLimit
-       , simulate
-       , forceLayout
+  , ForceLayoutOpts(..)
+  , damping, energyLimit, stepLimit
+  , simulate
+  , forceLayout
 
-         -- * Internals
+    -- * Internals
 
-       , ensembleStep
-       , particleStep
-       , recalcForces
-       , kineticEnergy
+  , ensembleStep
+  -- , particleStep
+  -- , recalcForces
+  , kineticEnergy
 
-       ) where
+  ) where
 
 import           Data.Default.Class
 import qualified Data.Foldable      as F
-import qualified Data.Map           as M
+-- import qualified Data.Map           as M
 import           Data.Monoid
+
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Mutable as M
 
 import           Control.Lens
 
-import           Linear.Affine
+import           Linear.Affine (Point (..), (.+^), (.-.))
 import           Linear.Metric
 import           Linear.Vector
+import           Linear.V2
 
 ------------------------------------------------------------
 --  Particles
@@ -100,17 +107,16 @@ import           Linear.Vector
 
 -- | A particle has a current position, current velocity, and current
 --   force acting on it.
-data Particle v n = Particle { _pos   :: Point v n
-                             , _vel   :: v n
-                             , _force :: v n
-                             }
-  deriving Eq
+data Particle v n = Particle
+  { _pos   :: !(Point v n)
+  , _vel   :: !(v n)
+  }
 
 makeLenses ''Particle
 
 -- | Create an initial particle at rest at a particular location.
 initParticle :: (Additive v, Num n) => Point v n -> Particle v n
-initParticle p = Particle p zero zero
+initParticle p = Particle p zero
 
 ------------------------------------------------------------
 --  Ensembles
@@ -122,14 +128,22 @@ type PID = Int
 -- | An edge is a pair of particle identifiers.
 type Edge = (PID, PID)
 
+type Forces v n = [(U.Vector (Int,Int), Point v n -> Point v n -> v n)]
+
+data Ensemble v n = Ensemble
+  { _forces    :: Forces v n
+  , _particles :: Vector (Particle v n)
+  }
+
 -- | An @Ensemble@ is a physical configuration of particles.  It
 --   consists of a mapping from particle IDs (unique integers) to
 --   particles, and a list of forces that are operative.  Each force
 --   has a list of edges to which it applies, and is represented by a
 --   function giving the force between any two points.
-data Ensemble v n = Ensemble { _forces    :: [([Edge], Point v n -> Point v n -> v n)]
-                             , _particles :: M.Map PID (Particle v n)
-                             }
+-- data Ensemble v n = Ensemble
+--   { _forces    :: [([Edge], Point v n -> Point v n -> v n)]
+--   , _particles :: M.Map PID (Particle v n)
+--   }
 
 makeLenses ''Ensemble
 
@@ -139,63 +153,88 @@ makeLenses ''Ensemble
 
 -- | Simulate one time step for an entire ensemble, with the given
 --   damping factor.
-ensembleStep :: (Additive v, Num n) => n -> Ensemble v n -> Ensemble v n
-ensembleStep d = (over particles . M.map) (particleStep d) . recalcForces
+-- ensembleStep
+--   :: (Additive v, Num n)
+--   => n            -- ^ damping
+--   -> Ensemble v n
+--   -> Ensemble v n
+-- ensembleStep d = (over particles . M.map) (particleStep d) . recalcForces
 
 -- | Simulate one time step for a particle (assuming the force acting
 --   on it has already been computed), with the given damping factor.
-particleStep :: (Additive v, Num n) => n -> Particle v n -> Particle v n
-particleStep d = stepPos . stepVel
-  where stepVel p = vel .~ (d *^ (p^.vel ^+^ p^.force)) $ p
-        stepPos p = pos %~ (.+^ p^.vel) $ p
+-- particleStep :: (Additive v, Num n) => n -> Particle v n -> Particle v n
+-- particleStep d = stepPos . stepVel
+--   where stepVel p = vel .~ (d *^ (p^.vel ^+^ p^.force)) $ p
+--         stepPos p = pos %~ (.+^ p^.vel) $ p
+
+-- [([Edge], Point v n -> Point v n -> v n)]
+
+
+ensembleStep
+  :: (Additive v, Num n)
+  => n            -- ^ damping
+  -> Ensemble v n
+  -> Ensemble v n
+ensembleStep d (Ensemble fs ps) = Ensemble fs ps'
+  where
+    ps' = V.imap updateParticle ps
+    updateParticle i p = Particle ((p^.pos) .+^ v') v'
+      where v' = d *^ (p^.vel ^+^ (fv V.! i))
+    n = V.length ps
+    -- the forces acting of each particle
+    fv = V.create $ do
+      fmv <- M.replicate n zero
+      F.for_ fs $ \(edges,f) ->
+        U.forM_ edges $ \(i1,i2) -> do
+          let !force = f (ps^?!ix i1.pos) (ps^?!ix i2.pos)
+          M.modify fmv (^+^ force) i1
+          M.modify fmv (^-^ force) i2
+      return fmv
+{-# INLINE ensembleStep #-}
 
 -- | Recalculate all the forces acting in the next time step of an
 --   ensemble.
-recalcForces :: (Additive v, Num n) => Ensemble v n -> Ensemble v n
-recalcForces = calcForces . zeroForces
-  where zeroForces = (particles %~) . M.map $ force .~ zero
-        calcForces (Ensemble fs ps)
-          = Ensemble fs
-            (ala Endo F.foldMap (concatMap (\(es, f) -> (map (mkForce f) es)) fs) ps)
-        -- mkForce :: (Point v n -> Point v n -> v n)
-        --         -> Edge
-        --         -> M.Map Int (Particle v n)
-        --         -> M.Map Int (Particle v n)
-        mkForce f (i1, i2) m
-          = case (M.lookup i1 m, M.lookup i2 m) of
-              (Just p1, Just p2) ->
-                ( M.adjust (force %~ (^+^ f (p1^.pos) (p2^.pos))) i1
-                . M.adjust (force %~ (^-^ f (p1^.pos) (p2^.pos))) i2)
-                m
-              _                  -> m
+-- calcForces :: (Additive v, Num n) => Ensemble v n -> Vector (v n)
+-- calcForces (Ensemble fs ps) = create $ do
+
+
+-- recalcForces = calcForces . zeroForces
+--   where zeroForces = (particles %~) . M.map $ force .~ zero
+--         calcForces (Ensemble fs ps)
+--           = Ensemble fs
+--             (ala Endo F.foldMap (concatMap (\(es, f) -> (map (mkForce f) es)) fs) ps)
+--         -- mkForce :: (Point v n -> Point v n -> v n)
+--         --         -> Edge
+--         --         -> M.Map Int (Particle v n)
+--         --         -> M.Map Int (Particle v n)
+--         mkForce f (i1, i2) m
+--           = case (M.lookup i1 m, M.lookup i2 m) of
+--               (Just p1, Just p2) ->
+--                 ( M.adjust (force %~ (^+^ f (p1^.pos) (p2^.pos))) i1
+--                 . M.adjust (force %~ (^-^ f (p1^.pos) (p2^.pos))) i2)
+--                 m
+--               _                  -> m
 
 -- | Compute the total kinetic energy of an ensemble.
 kineticEnergy :: (Metric v, Num n) => Ensemble v n -> n
-kineticEnergy = ala Sum F.foldMap . fmap (quadrance . view vel) . view particles
+kineticEnergy = sumOf (particles . folded . vel . to quadrance)
+{-# INLINE kineticEnergy #-}
 
 ------------------------------------------------------------
 --  Simulation
 ------------------------------------------------------------
 
 -- | Options for customizing a simulation.
-data ForceLayoutOpts n =
-  FLOpts
-  { _damping     :: n         -- ^ Damping factor to be
-                              --   applied at each step.
-                              --   Should be between 0 and 1.
-                              --   The default is 0.8.
-  , _energyLimit :: Maybe n   -- ^ Kinetic energy below which
-                              --   simulation should stop.
-                              --   If @Nothing@, pay no
-                              --   attention to kinetic
-                              --   energy.  The default is
-                              --   @Just 0.001@.
-  , _stepLimit   :: Maybe Int -- ^ Maximum number of
-                              --   simulation steps.  If
-                              --   @Nothing@, pay no
-                              --   attention to the number of
-                              --   steps.  The default is
-                              --   @Just 1000@.
+data ForceLayoutOpts n = FLOpts
+  { _damping     :: n
+    -- ^ Damping factor to be applied at each step. Should be between 0
+    --   and 1. The default is 0.8.
+  , _energyLimit :: Maybe n
+    -- ^ Kinetic energy below which simulation should stop. If @Nothing@,
+    --   pay no attention to kinetic energy. The default is @Just 0.001@.
+  , _stepLimit   :: Maybe Int
+    -- ^ Maximum number of simulation steps.  If @Nothing@, pay no
+    --   attention to the number of steps.  The default is @Just 1000@.
   }
 
 makeLenses ''ForceLayoutOpts
@@ -222,6 +261,7 @@ simulate opts e
   . drop 1
   . iterate (ensembleStep (opts ^. damping))
   $ e
+{-# INLINE simulate #-}
 
 -- | Run a simluation from a starting ensemble, yielding either the
 --   first ensemble to have kinetic energy below the 'energyLimit' (if
@@ -231,6 +271,7 @@ simulate opts e
 forceLayout :: (Metric v, Num n, Ord n)
             => ForceLayoutOpts n -> Ensemble v n -> Ensemble v n
 forceLayout opts = last . simulate opts
+{-# INLINE forceLayout #-}
 
 ------------------------------------------------------------
 --  Standard forces
@@ -243,15 +284,18 @@ forceLayout opts = last . simulate opts
 distForce :: (Metric v, Floating n) => (n -> n) -> Point v n -> Point v n -> v n
 distForce f p1 p2 = withLength (f (distance p1 p2)) (p2 .-. p1)
   where withLength s v = s *^ signorm v
+{-# INLINE distForce #-}
 
 -- | @hookeForce k l p1 p2@ computes the force on @p1@, assuming that
 --   @p1@ and @p2@ are connected by a spring with equilibrium length @l@
 --   and spring constant @k@.
 hookeForce :: (Metric v, Floating n) => n -> n -> Point v n -> Point v n -> v n
 hookeForce k l = distForce (\d -> k * (d - l))
+{-# INLINE hookeForce #-}
 
 -- | @coulombForce k@ computes the electrostatic repulsive force
 --   between two charged particles, with constant of proportionality
 --   @k@.
 coulombForce :: (Metric v, Floating n) => n -> Point v n -> Point v n -> v n
 coulombForce k = distForce (\d -> -k/(d*d))
+{-# INLINE coulombForce #-}
